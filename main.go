90148fbcc2795a4bc5de0e874892f000e6065b68
Brian MAcias <brian.macias3872@alumnos.udg.mx>
2020-09-29 14:25:36 -0500

    prueba01

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var ciclos int
+	var e float64 = 0.0
+
+	fmt.Scan(&ciclos)
+
+	for i := 0; i <= ciclos; i++ {
+		e += 1.0 / float64(factorial(uint(i)))
+	}
+
+	fmt.Println(e)
+}
+
+func factorial(n uint) uint {
+	if n == 0 {
+		return 1
+	}
+	var f uint = 1
+	var i uint
+	for i = 1; i <= n; i++ {
+		f *= i
+	}
+	return f
 }
